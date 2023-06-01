8659a9797a46b3b8cddf274629e0558c95820671
Mr. Robot <gomexxxx71@gmail.com>
2023-06-01 20:05:38 +0600

    chore: firebase setup

---
+++ src/services/firebase.ts
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
+
+firebase.initializeApp(firebaseConfig);
+const DB = firebase.firestore();
+const matchesCollection = DB.collection("matches");
+const playersCollection = DB.collection("players");
+const positionsCollection = DB.collection("positions");
+const promotionsCollection = DB.collection("promotions");
+const teamsCollection = DB.collection("teams");
+
+export {
+  firebase,
+  matchesCollection,
+  playersCollection,
+  positionsCollection,
+  promotionsCollection,
+  teamsCollection,
+};
