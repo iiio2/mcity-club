ee98da70ebe69cc31f03f01cec74ac8cdd4d502e
Mr. Robot <gomexxxx71@gmail.com>
2023-06-02 20:32:25 +0600

    feat: header

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,35 @@
+import { AppBar, Toolbar, Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { CityLogo } from "../../utils/tools";
+
+const Header = () => {
+  return (
+    <AppBar
+      position="fixed"
+      style={{
+        backgroundColor: "#98c5e9",
+        boxShadow: "none",
+        padding: "10px 0",
+        borderBottom: "2px solid #00285e",
+      }}
+    >
+      <Toolbar style={{ display: "flex" }}>
+        <div style={{ flexGrow: 1 }}>
+          <div className="header_logo">
+            <CityLogo link={true} linkTo={"/"} width="70px" height="70px" />
+          </div>
+        </div>
+
+        <Link to="/the_matches">
+          <Button color="inherit">Matches</Button>
+        </Link>
+
+        <Link to="/the_team">
+          <Button color="inherit">The team</Button>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
---
+++ src/utils/tools.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import mcitylogo from "../resources/images/logos/manchester_city_logo.png";
+
+export const CityLogo = (props: any) => {
+  const template = (
+    <div
+      className="img_cover"
+      style={{
+        width: props.width,
+        height: props.height,
+        background: `url(${mcitylogo}) no-repeat`,
+      }}
+    ></div>
+  );
+
+  if (props.link) {
+    return (
+      <Link className="link_logo" to={props.linkTo}>
+        {template}
+      </Link>
+    );
+  } else {
+    return template;
+  }
+};
