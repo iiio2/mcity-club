47b65ad1e9f0453c0cebbea44d59ad9f5560355c
Mr. Robot <gomexxxx71@gmail.com>
2023-06-02 22:56:39 +0600

    feat: build meet players

--- src/components/home/Index.tsx
+++ src/components/home/Index.tsx
@@ -1,11 +1,13 @@
 import Featured from "./featured/Index";
 import Matches from "./matches/Index";
+import MeetPlayers from "./meetPlayers/Index";
 
 const Home = () => {
   return (
     <>
       <Featured />
       <Matches />
+      <MeetPlayers />
     </>
   );
 };
