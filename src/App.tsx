7d426eecd7e5f5b7025e06790cd1cf9413a5be8a
Mr. Robot <gomexxxx71@gmail.com>
2023-06-03 11:27:00 +0600

    feat: admin matches

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import SignIn from "./components/signIn/Index";
 import TheMatches from "./components/theMatches/Index";
 import TheTeam from "./components/theTeam/Index";
 import Dashboard from "./components/admin/Dashboard";
+import AdminMatches from "./components/admin/matches/Index";
 import NotFound from "./components/notFound";
 
 function App({ user }: any) {
@@ -20,6 +21,7 @@ function App({ user }: any) {
         <Route path="/the_matches" element={<TheMatches />} />
         <Route path="/the_team" element={<TheTeam />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="admin_matches" element={<AdminMatches />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
