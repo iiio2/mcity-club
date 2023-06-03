ff63bb93bdde7b9535e7bad39f1f44ba927e5b2d
Mr. Robot <gomexxxx71@gmail.com>
2023-06-03 10:52:13 +0600

    feat: admin layout

---
+++ src/hoc/AdminLayout.tsx
@@ -0,0 +1,17 @@
+import AdminNav from "../components/admin/nav/AdminNav";
+
+const AdminLayout = (props: any) => {
+  return (
+    <div className="admin_container">
+      <div className="admin_left_nav">
+        <AdminNav />
+      </div>
+      <div className="admin_right">
+        <h2>{props.title}</h2>
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+export default AdminLayout;
